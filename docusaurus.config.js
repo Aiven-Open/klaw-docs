53dd96a2ff24e57a8190e7ca6826eddf2d0a39ae
muralibasani <muralidahr.basani@aiven.io>
2023-10-30 14:40:47 +0100

    Adding google analytics tag
    Signed-off-by: muralibasani <muralidahr.basani@aiven.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,6 +47,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: 'G-DCVBZR4EQV',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
