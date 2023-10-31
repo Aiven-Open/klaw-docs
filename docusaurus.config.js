000d135d778f2af349ea3b617059b112cab26b4d
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-10-31 15:53:41 +0100

    Add snowplow script.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
+  clientModules: [require.resolve("./snowplow.js")],
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
