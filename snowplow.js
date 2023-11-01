1f6efc185681487b91600cdf6f6776b0b626514c
Mirjam Aulbach <mirjam.aulbach@aiven.io>
2023-11-01 10:26:13 +0100

    Don't set tracker on dev env.
    Signed-off-by: Mirjam Aulbach <mirjam.aulbach@aiven.io>

--- snowplow.js
+++ snowplow.js
@@ -1,6 +1,7 @@
 import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 import { newTracker, trackPageView } from "@snowplow/browser-tracker";
 
+const isProduction = process.env.NODE_ENV === "production";
 const trackerConfig = {
   appId: "klaw-docs",
   platform: "web",
@@ -23,17 +24,22 @@ function setupBrowserTracker() {
   newTracker("at", "dc.aiven.io", trackerConfig);
 }
 
-if (ExecutionEnvironment.canUseDOM) {
+if (isProduction && ExecutionEnvironment.canUseDOM) {
+  // only set tracker on prod
   setupBrowserTracker();
 }
 
 const module = {
   onRouteDidUpdate({ location, previousLocation }) {
-    if (location.pathname !== previousLocation?.pathname) {
-      // see https://github.com/facebook/docusaurus/pull/7424 regarding setTimeout
-      setTimeout(() => {
-        trackPageView();
-      });
+    // only set tracker on prod
+    if (isProduction) {
+      // only call trackPageView when page route changed
+      if (location.pathname !== previousLocation?.pathname) {
+        // see https://github.com/facebook/docusaurus/pull/7424 regarding setTimeout
+        setTimeout(() => {
+          trackPageView();
+        });
+      }
     }
   },
 };
